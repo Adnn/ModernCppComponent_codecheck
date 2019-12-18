72ab8217b796059f17a8bd27bc3c3ac1b1ba14fc
Adrien <Adnn@users.noreply.github.com>
2019-12-19 00:14:06 +0430

    Check code for section "Using upstream dependencies: CMake"

--- proof.sh
+++ proof.sh
@@ -7,6 +7,6 @@ rm -r build/*
 
 pushd build
 #cmake .. -DCMAKE_CXX_STANDARD=14 -DCMAKE_VERBOSE_MAKEFILE=ON
-cmake .. -DBUILD_tests=ON
+cmake .. -DBUILD_tests=ON $@
 cmake --build .
 popd
---
+++ src/libs/B/B/Foo.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace myrepo {
+
+struct Foo
+{
+    Foo(int aFoo) :
+        mFoovalue(aFoo)
+    {}
+
+    int mFoovalue;
+};
+
+} // namespace myrepo
