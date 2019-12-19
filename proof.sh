b4d1712623a70b7491f2247ad96cbc2d508d6327
Adrien <Adnn@users.noreply.github.com>
2019-12-20 00:10:29 +0430

    Check code for section "Preparing a CMake package"

--- proof.sh
+++ proof.sh
@@ -2,11 +2,43 @@
 
 set -e
 
-mkdir -p build
-rm -r build/*
+##
+## Build the demo repository
+##
+WORKDIR=build
+mkdir -p ${WORKDIR}
+rm -r ${WORKDIR}
+mkdir -p ${WORKDIR}
 
-pushd build
+pushd ${WORKDIR}
+BUILD_DIR=$(pwd)
 #cmake .. -DCMAKE_CXX_STANDARD=14 -DCMAKE_VERBOSE_MAKEFILE=ON
-cmake .. -DBUILD_tests=ON $@
+cmake .. -DBUILD_tests=ON -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/SDK/myrepository $@
+cmake --build . --target install
+popd
+
+##
+## Build a downstream consumer, mocking-up a separate repository
+##
+
+# From build tree
+WORKDIR=build-downstream
+mkdir -p ${WORKDIR}
+rm -r ${WORKDIR}
+mkdir -p ${WORKDIR}
+
+pushd ${WORKDIR}
+cmake ../downstream -DA_DIR=${BUILD_DIR} -DB_DIR=${BUILD_DIR} $@
+cmake --build .
+popd
+
+# From install tree
+WORKDIR=build-downstream-install
+mkdir -p ${WORKDIR}
+rm -r ${WORKDIR}
+mkdir -p ${WORKDIR}
+
+pushd ${WORKDIR}
+cmake ../downstream -DA_DIR=${BUILD_DIR}/SDK/myrepository/lib/cmake/A -DB_DIR=${BUILD_DIR}/SDK/myrepository/lib/cmake/B $@
 cmake --build .
 popd
