b7f216d1d0f964406f9b8d9b586ea4655c9d7d3c
Adrien <Adnn@users.noreply.github.com>
2019-12-20 00:10:29 +0430

    Check code for section "Preparing a CMake package"

---
+++ downstream/src/apps/consumer/consumer/main.cpp
@@ -0,0 +1,25 @@
+#include <A/accumulate.h>
+#include <A/FooBar.h>
+#include <A/is_directory.h>
+
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    int value = 3;
+    std::cout << "Accumulation of " << value << ": "
+              << myrepo::accumulate(value)
+              << std::endl;
+
+    if (argc >= 2)
+    {
+        std::cout << "Is first argument '" << argv[1] << "' a directory? "
+                  << std::boolalpha
+                  << myrepo::is_directory(argv[1])
+                  << std::endl;
+    }
+
+    myrepo::FooBar fb{1, 2};
+
+    exit(EXIT_SUCCESS);
+}
