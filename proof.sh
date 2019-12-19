3ad1e57619577297b49a8a5f48c6caf2027c3847
Adrien <Adnn@users.noreply.github.com>
2019-12-20 00:10:29 +0430

    Check code for section "Preparing a CMake package"

--- proof.sh
+++ proof.sh
@@ -27,7 +27,34 @@ mkdir -p ${WORKDIR}
 rm -r ${WORKDIR}
 mkdir -p ${WORKDIR}
 
-pushd build
-cmake .. -DBUILD_tests=ON -DBoost_ROOT=${BOOST_ROOT}
+pushd ${WORKDIR}
+BUILD_DIR=$(pwd)
+cmake .. -DBUILD_tests=ON -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/SDK/myrepository -DBoost_ROOT=${BOOST_ROOT}
+cmake --build . --target install
+popd
+
+##
+## Build a downstream consumer, mocking-up a separate repository
+##
+
+# From build tree
+WORKDIR=build-downstream
+mkdir -p ${WORKDIR}
+rm -r ${WORKDIR}
+mkdir -p ${WORKDIR}
+
+pushd ${WORKDIR}
+cmake ../downstream -DCMAKE_PREFIX_PATH=${BUILD_DIR} -DBoost_ROOT=${BOOST_ROOT}
+cmake --build .
+popd
+
+# From install tree
+WORKDIR=build-downstream-install
+mkdir -p ${WORKDIR}
+rm -r ${WORKDIR}
+mkdir -p ${WORKDIR}
+
+pushd ${WORKDIR}
+cmake ../downstream -DCMAKE_PREFIX_PATH=${BUILD_DIR}/SDK/myrepository -DBoost_ROOT=${BOOST_ROOT}
 cmake --build .
 popd
